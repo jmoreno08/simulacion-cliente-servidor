32572dd1c4adc66a971828781dcd781edc0ec2dd
Jonathan Moreno <43528500+jmoreno08@users.noreply.github.com>
2026-05-25 17:37:40 -0500

    Crear constants.py para configuraciones compartidas

---
+++ app/common/constants.py
@@ -0,0 +1,7 @@
+# Configuración compartida
+"""
+Aquí se establecerán todas las constantes globales o configuraciones compartidas
+que servirán tanto al cliente como al servidor, por ejemplo, IP y puerto.
+"""
+SERVER_HOST = '127.0.0.1'  # localhost
+SERVER_PORT = 12345  # Puerto predeterminado
